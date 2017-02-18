5b377bc1a7d8dc56d6cc024533c738463d6dd80c
Revolutio <mail@philipp-schwab.de>
2017-02-18 18:53:56 +0100

    add comments

--- kachel.ino
+++ kachel.ino
@@ -1,27 +1,7 @@
-// - - - - -
-// DmxSerial - A hardware supported interface to DMX.
-// DmxSerialRecv.ino: Sample DMX application for retrieving 3 DMX values:
-// address 1 (red) -> PWM Port 9
-// address 2 (green) -> PWM Port 6
-// address 3 (blue) -> PWM Port 5
-// 
-// Copyright (c) 2011-2015 by Matthias Hertel, http://www.mathertel.de
-// This work is licensed under a BSD style license. See http://www.mathertel.de/License.aspx
-// 
-// Documentation and samples are available at http://www.mathertel.de/Arduino
-// 25.07.2011 creation of the DmxSerial library.
-// 10.09.2011 fully control the serial hardware register
-//            without using the Arduino Serial (HardwareSerial) class to avoid ISR implementation conflicts.
-// 01.12.2011 include file and extension changed to work with the Arduino 1.0 environment
-// 28.12.2011 changed to channels 1..3 (RGB) for compatibility with the DmxSerialSend sample.
-// 10.05.2012 added some lines to loop to show how to fall back to a default color when no data was received since some time.
-// - - - - -
-
 #include <DMXSerial.h>
 #include "Panel.h"
 
-// Constants for demo program
-
+//Setting up dmx channels. The argument is the startaddress. 
 Panel one(1);
 Panel two(7);
 
